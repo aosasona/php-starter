a925f9db33c8e6b20f98af7fb443e8b959747c56
Ayodeji O <osasonaayodeji1@gmail.com>
2022-08-14 04:17:20 +0100

    Update commands

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -2,22 +2,49 @@
 
 namespace Trulyao\PhpJwt\Controllers;
 
+use Exception;
 use Trulyao\PhpRouter\Helper\Response as Response;
 use Trulyao\PhpRouter\Helper\Request as Request;
 
 class AuthController {
+
     public static function sign_up (Request $request, Response $response): Response
     {
-        extract($request->body());
+        try {
+            extract($request->body());
 
-        if ($password !== $confirm_password) {
-            return $response->status(400)->json([
+            if (empty($full_name) || empty($email) || empty($password) || empty($confirm_password)) {
+                return $response->status(400)->json([
+                    "success" => false,
+                    "message" => "Please fill all fields"
+                ]);
+            }
+
+            $name = explode(" ", $full_name);
+
+            [$first_name, $last_name] = $name + [null, null];
+
+            if (empty($first_name) || empty($last_name)) {
+                return $response->json([
+                    "success" => false,
+                    "message" => "First name and last name are required"
+                ])->status(400);
+            }
+
+            if ($password !== $confirm_password) {
+                return $response->json([
+                    "success" => false,
+                    "message" => "Passwords do not match"
+                ])->status(400);
+            }
+
+            return $response->json($request->body());
+        } catch (Exception $e) {
+            return $response->status(500)->json([
                 "success" => false,
-                "message" => "Passwords do not match"
+                "message" => "Server error!"
             ]);
         }
-
-        return $response->json($request->body());
     }
 
     public static function sign_in (Request $request, Response $response): Response
@@ -26,4 +53,5 @@ class AuthController {
 
         return $response->json($request->body());
     }
+
 }
